005e0f414620f5729684f98de8eab9695f19a8c2
Syo Miyata <syo030078@users.noreply.github.com>
2026-03-12 16:57:14 +0900

    feat: add cat icon to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,20 @@
-import { Box, Flex, Link } from '@chakra-ui/react';
+import { Box, Flex, Image, Link } from '@chakra-ui/react';
 
 export default function Header() {
   return (
     <header>
-      <Flex as="nav" align="center" justify="space-between" p={4}>
-        <Box>
+      <Flex as="nav" align="center" justify="space-between" py="1rem" px={0} maxW="36rem" mx="auto">
+        <Flex align="center" gap="0.5rem">
+          <Image
+            borderRadius="full"
+            boxSize="28px"
+            src="/images/cat-icon.svg"
+            alt="cat icon"
+          />
           <Link href="/" fontSize="xl" fontWeight="bold">
             Syo Miyata
           </Link>
-        </Box>
+        </Flex>
         <Box>
           <Link href="https://github.com/syo030078" isExternal>
             GitHub
